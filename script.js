26896569fbbffa33fe2883722242db01f2507d84
louizenoval <155514230+louizenoval@users.noreply.github.com>
2024-01-20 12:08:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeButtonColor(buttonId) {
+    var button = document.getElementById(buttonId);
+    button.classList.toggle("clicked");
+    
+}
+
+function changeTableColor(tableId) {
+    var table = document.getElementById(tableId);
+    table.classList.toggle("clicked");
+}
